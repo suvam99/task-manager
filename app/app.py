735666f288d70e2cde7f19e188a776eaeca31516
suvam99 <suvambhunia020@gmail.com>
2026-02-15 15:11:56 +0000

    setup basic Flask application

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/health", methods=["GET"])
+def health():
+    return {"status": "ok"}, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
